c2d60e0bf0351758d232dea4faaa0af0aaad4752
cotelCatherine <bezmozggoff@gmail.com>
2026-03-04 12:07:46 +0700

    Initial commit

---
+++ src/main/java/ru/itk/mvclibrary/MvcLibraryApplication.java
@@ -0,0 +1,13 @@
+package ru.itk.mvclibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/itk/mvclibrary/MvcLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itk.mvclibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
